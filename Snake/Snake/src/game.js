00485901e700d567cbcd24199b94974029c494d2
cgray6 <cgray6@E1116W20>
2017-09-25 11:20:45 -0500

    updates to software architecture

---
+++ Snake/Snake/src/game.js
@@ -0,0 +1,30 @@
+﻿//game.js
+
+import Snake from "./snake";
+
+/** @class Game
+    represents logic of snake game
+*/
+class Game
+{
+    constructor()
+    {
+        this.snake = new Snake();
+        this.food = [];
+        //make canvas
+        this.canvas = document.createElement("canvas");
+        this.canvas.width = 100;
+        this.canvas.height = 100;
+        document.body.appendChild(canvas);
+        this.context = canvas.getContext("2d");
+
+    }
+    update()
+    {
+        this.snake.update();
+    }
+    render()
+    {
+        this.snake.render(context);
+    }
+}
---
+++ Snake/Snake/src/snake.js
@@ -0,0 +1,48 @@
+﻿//snake.js
+
+/** @class Snake
+    the snake in a snake game
+*/
+module.exports = Snake;
+
+class Snake
+{
+    constructor(x, y, segments)
+    {
+        this.body = [];
+        for(var i = 0; i < segments; i++)
+        {
+            this.body.push({
+                x: x - i,
+                y: y
+            });
+        }
+        this.direction = "right";
+        thissegmentSize = 15;
+    }
+    checkForConsumption(food)
+    {
+
+    }
+    update()
+    {
+        //did we smack a wall
+        //did we eat food
+        //did we eat outselves
+        //do we need to grow
+
+    }
+    /**
+        @function render
+    */
+    render(ctx)
+    {
+        
+        this.body.forEach(function (segement) {
+            ctx.save();
+            ctx.fillStyle = "green";
+            ctx.fillRect(segment.x * this.Segmentsize, segment.y * this.segmentSize, 1, 1);
+            ctx.restore();
+        });
+    }
+}
